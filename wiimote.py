0e966ec9c12e83c7fcba6504d3c7f02dee75326a
Patrick Hennis <patrickhennis@gmail.com>
2016-04-02 19:37:51 -0400

    remove global

--- wiimote.py
+++ wiimote.py
@@ -5,14 +5,14 @@ import time
 
 def makeConnection():
   print "Press 1 + 2 on the wiimote yo"
-  global wm
   wm = cwiid.Wiimote()
   wm.led = 1
   wm.enable(cwiid.FLAG_MOTIONPLUS)
   wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_MOTIONPLUS
   print "you be connected"
+  return wm
 
-def determine():
+def determine(wm):
   if wm.state['buttons'] & cwiid.BTN_PLUS:
     #takeoff
     print "get lifted"
@@ -24,32 +24,32 @@ def determine():
     print "kill everything"
   if wm.state['buttons'] & cwiid.BTN_B:
     #flip
-    print "barrell roll"
+	print "barrell roll"
   if wm.state['buttons'] & cwiid.BTN_UP:
-    #foward
-    print "movin on forward"
+	#foward
+	print "movin on forward"
   if wm.state['buttons'] & cwiid.BTN_DOWN:
-    #back
-    print "back it up"
+	#back
+	print "back it up"
   if wm.state['buttons'] & cwiid.BTN_LEFT:
-    #left
-    print "to the left"
+	#left
+	print "to the left"
   if wm.state['buttons'] & cwiid.BTN_RIGHT:
-    #right
-    print "to the right now yall"
+	#right
+	print "to the right now yall"
   if wm.state['buttons'] & cwiid.BTN_1:
-    #elevate 
-    print "raise it"
+	#elevate
+	print "raise it"
   if wm.state['buttons'] & cwiid.BTN_2:
-    #lower
-    print "drop it low"
+	#lower
+	print "drop it low"
   time.sleep(0.1)
 
 def main():
-  makeConnection()
+  wm2 = makeConnection()
   while True:
     wm.led = (wm.state['led'] + 1) % 16
-    determine()
+    determine(wm2)
 
 if __name__ == "__main__":
   main()
