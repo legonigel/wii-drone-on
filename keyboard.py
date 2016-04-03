bb61bbc057426bc407cc5df001c8d9ab7a075360
Legonigel <legonigel@gmail.com>
2016-04-03 01:39:05 -0400

    fix current command in keyboard

--- keyboard.py
+++ keyboard.py
@@ -2,10 +2,9 @@ import time
 from pygame import *
 import pygame
 import threading
-from Queue import Queue
 
 class InputKeyboard(threading.Thread):
-	event_queue = Queue()
+	current_command = ""
 	def __init__(self, nom = ''):
 		threading.Thread.__init__(self)
 		self._stopevent = threading.Event()
@@ -20,76 +19,77 @@ class InputKeyboard(threading.Thread):
 
 		running = True
 
-		current_command = ""
+		self.current_command = ""
 
 		while running:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
 				elif event.type == pygame.KEYUP:
-					current_command = ""
+					self.current_command = ""
 				elif event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_ESCAPE:
-						current_command = "reset"
+						self.current_command = "reset"
 						running = False
 						# takeoff / land
 					elif event.key == pygame.K_RETURN:
-						current_command = "takeoff"
+						self.current_command = "takeoff"
 					elif event.key == pygame.K_SPACE:
-						current_command = "land"
+						self.current_command = "land"
 						# emergency
 					elif event.key == pygame.K_BACKSPACE:
-						current_command = "reset"
+						self.current_command = "reset"
 						# forward / backward
 					elif event.key == pygame.K_w:
-						current_command = "forward"
+						self.current_command = "forward"
 					elif event.key == pygame.K_s:
-						current_command = "back"
+						self.current_command = "back"
 						# left / right
 					elif event.key == pygame.K_a:
-						current_command = "left"
+						self.current_command = "left"
 					elif event.key == pygame.K_d:
-						current_command = "right"
+						self.current_command = "right"
 						# up / down
 					elif event.key == pygame.K_UP:
-						current_command = "up"
+						self.current_command = "up"
 					elif event.key == pygame.K_DOWN:
-						current_command = "down"
+						self.current_command = "down"
 						# turn left / turn right
 					elif event.key == pygame.K_LEFT:
-						current_command = "turn_left"
+						self.current_command = "turn_left"
 					elif event.key == pygame.K_RIGHT:
-						current_command = "turn_right"
+						self.current_command = "turn_right"
 						# speed
 					elif event.key == pygame.K_1:
-						current_command = "speed 0.1"
+						self.current_command = "speed 0.1"
 					elif event.key == pygame.K_2:
-						current_command = "speed 0.2"
+						self.current_command = "speed 0.2"
 					elif event.key == pygame.K_3:
-						current_command = "0.3"
+						self.current_command = "0.3"
 					elif event.key == pygame.K_4:
-						current_command = "0.4"
+						self.current_command = "0.4"
 					elif event.key == pygame.K_5:
-						current_command = "0.5"
+						self.current_command = "0.5"
 					elif event.key == pygame.K_6:
-						current_command = "0.6"
+						self.current_command = "0.6"
 					elif event.key == pygame.K_7:
-						current_command = "0.7"
+						self.current_command = "0.7"
 					elif event.key == pygame.K_8:
-						current_command = "0.8"
+						self.current_command = "0.8"
 					elif event.key == pygame.K_9:
-						current_command = "0.9"
+						self.current_command = "0.9"
 					elif event.key == pygame.K_0:
-						current_command = "1.0"
+						self.current_command = "1.0"
+					print "cur_cmd: {}".format(self.current_command)
 
 	def makeConnection(self):
-		pass
+		self.start()
 
 	def determine(self):
-		return current_command
+		return self.current_command
 
 def main():
-	myInput = InputKeyboar()
+	myInput = InputKeyboard()
 	myInput.makeConnection()
 	while True:
 		print myInput.determine()
