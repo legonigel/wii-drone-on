4b0d62411ddbbaf5aab1468e18b9eabfe97bc7d0
Legonigel <legonigel@gmail.com>
2016-04-02 23:17:39 -0400

    fix object wiimote

--- wiimote.py
+++ wiimote.py
@@ -2,59 +2,59 @@
 
 import cwiid
 import time
-class InputWiimote(Object):
-    def makeConnection(self):
-      print "Press 1 + 2 on the wiimote yo"
-      wm = cwiid.Wiimote()
-      wm.led = 1
-      wm.enable(cwiid.FLAG_MOTIONPLUS)
-      wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_MOTIONPLUS
-      print "you be connected"
-      self.wm = wm
-      return wm
+class InputWiimote(object):
+	def makeConnection(self):
+	  print "Press 1 + 2 on the wiimote yo"
+	  wm = cwiid.Wiimote()
+	  wm.led = 1
+	  wm.enable(cwiid.FLAG_MOTIONPLUS)
+	  wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_MOTIONPLUS
+	  print "you be connected"
+	  self.wm = wm
+	  return wm
 
-    def determine(self):
-      command = ""
-      if self.wm.state['buttons'] & cwiid.BTN_PLUS:
-        #takeoff
-        command = "takeoff"
-      if self.wm.state['buttons'] & cwiid.BTN_MINUS:
-        #land
-        command = "land"
-      if self.wm.state['buttons'] & cwiid.BTN_HOME:
-        #panic and stop
-        command = "kill"
-      if self.wm.state['buttons'] & cwiid.BTN_B:
-        #flip
-    	command = "flip"
-      if self.wm.state['buttons'] & cwiid.BTN_UP:
-    	#foward
-    	command = "forward"
-      if self.wm.state['buttons'] & cwiid.BTN_DOWN:
-    	#back
-    	command = "backward"
-      if self.wm.state['buttons'] & cwiid.BTN_LEFT:
-    	#left
-    	command = "left"
-      if self.wm.state['buttons'] & cwiid.BTN_RIGHT:
-    	#right
-    	command = "right"
-      if self.wm.state['buttons'] & cwiid.BTN_1:
-    	#elevate
-    	command = "up"
-      if self.wm.state['buttons'] & cwiid.BTN_2:
-    	#lower
-    	command = "down"
-      time.sleep(0.1)
-      return command
+	def determine(self):
+	  command = ""
+	  if self.wm.state['buttons'] & cwiid.BTN_PLUS:
+		#takeoff
+		command = "takeoff"
+	  if self.wm.state['buttons'] & cwiid.BTN_MINUS:
+		#land
+		command = "land"
+	  if self.wm.state['buttons'] & cwiid.BTN_HOME:
+		#panic and stop
+		command = "kill"
+	  if self.wm.state['buttons'] & cwiid.BTN_B:
+		#flip
+		command = "flip"
+	  if self.wm.state['buttons'] & cwiid.BTN_UP:
+		#foward
+		command = "forward"
+	  if self.wm.state['buttons'] & cwiid.BTN_DOWN:
+		#back
+		command = "backward"
+	  if self.wm.state['buttons'] & cwiid.BTN_LEFT:
+		#left
+		command = "left"
+	  if self.wm.state['buttons'] & cwiid.BTN_RIGHT:
+		#right
+		command = "right"
+	  if self.wm.state['buttons'] & cwiid.BTN_1:
+		#elevate
+		command = "up"
+	  if self.wm.state['buttons'] & cwiid.BTN_2:
+		#lower
+		command = "down"
+	  time.sleep(0.1)
+	  return command
 
 
 def main():
   myInput = InputWiimote()
   wiimote = myInput.makeConnection()
   while True:
-    wiimote.led = (wiimote.state['led'] + 1) % 16
-    print myInput.determine()
+	wiimote.led = (wiimote.state['led'] + 1) % 16
+	print myInput.determine()
 
 if __name__ == "__main__":
   main()
--- wiimoteAC.py
+++ wiimoteAC.py
@@ -2,30 +2,30 @@
 
 import cwiid
 import time
-class InputWiimoteAC(Object):
-    def makeConnection(self):
-      print "Press 1 + 2 on the wiimote yo"
-      wm = cwiid.Wiimote()
-      wm.led = 1
-      wm.enable(cwiid.FLAG_MOTIONPLUS)
-      wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
-      print "you be connected"
-      self.wm = wm
-      return wm
+class InputWiimoteAC(object):
+	def makeConnection(self):
+	  print "Press 1 + 2 on the wiimote yo"
+	  wm = cwiid.Wiimote()
+	  wm.led = 1
+	  wm.enable(cwiid.FLAG_MOTIONPLUS)
+	  wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+	  print "you be connected"
+	  self.wm = wm
+	  return wm
 
-    def determine(self):
-      #print "x: " + str(float((wm.state['acc'][0]-125))/30)
-      #print "y: " + str(float((wm.state['acc'][1]-125))/30)
-      directions = "LR " + str(float((self.wm.state['acc'][0]-125))/30) + " FB " + str(float((self.wm.state['acc'][1]-125))/30)
-      time.sleep(0.1)
-      return directions
+	def determine(self):
+	  #print "x: " + str(float((wm.state['acc'][0]-125))/30)
+	  #print "y: " + str(float((wm.state['acc'][1]-125))/30)
+	  directions = "LR " + str(float((self.wm.state['acc'][0]-125))/30) + " FB " + str(float((self.wm.state['acc'][1]-125))/30)
+	  time.sleep(0.1)
+	  return directions
 
 def main():
   myInputAC = InputWiimoteAC()
   wiimote = myInputAC.makeConnection()
   while True:
-    wiimote.led = (wiimote.state['led'] + 1) % 16
-    print myInputAC.determine(wiimote)
+	wiimote.led = (wiimote.state['led'] + 1) % 16
+	print myInputAC.determine(wiimote)
 
 if __name__ == "__main__":
   main()
