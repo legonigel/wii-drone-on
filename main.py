6220e9f8388f5cefd5a71b2fa621fb86d7f5ffca
Legonigel <legonigel@gmail.com>
2016-04-02 23:48:47 -0400

    First commit of main

---
+++ main.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python
+# Python AR.Drone control
+# Copyright (C) 2016 LegoNigel <legonigel@gmail.com>
+
+from pygame import *
+import pygame
+import libardrone
+from drone_control import DroneController
+from video import Video
+from wiimote import InputWiimote
+
+import signal
+import sys
+
+def main():
+	"""Main program"""
+	pygame.init()
+	clock = pygame.time.Clock()
+	screen = pygame.display.set_mode((640,400))
+
+	controller = DroneController()
+	controller.start()
+
+	input_sources = []
+	input_sources.append(InputWiimote())
+
+	for source in input_sources:
+		if hasattr(source, "makeConnection"):
+			source.makeConnection()
+		else:
+			print "This input source has not makeConnection method you dumb coder!"
+			print source
+		if not hasattr(source, "determine"):
+			print "{} has not function determine, exiting".format(source)
+			exit(1)
+
+	running = True
+	try:
+		while running:
+			for source in input_sources:
+				command = source.determine()
+				if command:
+					controller.do_command(command)
+
+
+	except (KeyboardInterrupt, SystemExit):
+		pass
+	controller.do_command("reset")
+	controller.do_command("halt")
+	print "Done!"
+	quit()
+
+
+if __name__ == '__main__':
+	try:
+		#vid = Video('Thread Video')
+		#vid.start()
+		main()
+	except Exception:
+		#vid.stop()
+		sys.exit()
